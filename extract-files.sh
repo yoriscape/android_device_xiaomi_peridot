46b20a6009a445235b2fd555e7ef7b79ea30d4d7
Michael Bestas <mkbestas@lineageos.org>
2024-07-08 08:56:25 +0300

    peridot: Remove media_codecs_google_c2*
    AOSP codecs can be used by default and it is not necessary to include
    them device side.
    
    Change-Id: I8227b06f0466b614f12fa9008402b92461ea3dd0

--- extract-files.sh
+++ extract-files.sh
@@ -81,7 +81,7 @@ function blob_fixup() {
             ;;
         vendor/etc/media_codecs.xml|vendor/etc/media_codecs_cliffs_v0.xml|vendor/etc/media_codecs_performance_cliffs_v0.xml)
             [ "$2" = "" ] && return 0
-            sed -i -E '/media_codecs_(google_audio|google_telephony|vendor_audio)/d' "${2}"
+            sed -i -E '/media_codecs_(google_audio|google_c2|google_telephony|vendor_audio)/d' "${2}"
             ;;
         # MiuiCamera
         system/lib64/libgui-xiaomi.so)
