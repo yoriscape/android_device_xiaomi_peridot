1450df0ef0fae98a8b2835e60083694104cd7763
raghavt20 <raghavtandon2000@gmail.com>
2024-07-16 13:33:03 +0530

    peridot: Import Miui Camera
    Change-Id: Ia5f4d6ac34d4cb6b1002330762ae9673b3a514f9

--- extract-files.sh
+++ extract-files.sh
@@ -79,6 +79,21 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libhidlbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libhidlbase_shim.so" "${2}"
             ;;
+        # MiuiCamera
+        system/lib64/libgui-xiaomi.so)
+            patchelf --set-soname libgui-xiaomi.so "${2}"
+            ;;
+        system/lib64/libcamera_algoup_jni.xiaomi.so|system/lib64/libcamera_mianode_jni.xiaomi.so)
+            patchelf --replace-needed libgui.so libgui-xiaomi.so "${2}"
+            ;;
+        system/priv-app/MiuiCamera/MiuiCamera.apk)
+            tmp_dir="${EXTRACT_TMP_DIR}/MiuiCamera"
+            apktool d -q "$2" -o "$tmp_dir" -f
+            grep -rl "com.miui.gallery" "$tmp_dir" | xargs sed -i 's|"com.miui.gallery"|"com.google.android.apps.photos"|g'
+            apktool b -q "$tmp_dir" -o "$2"
+            rm -rf "$tmp_dir"
+            split --bytes=20M -d "$2" "$2".part
+            ;;
     esac
 }
 
