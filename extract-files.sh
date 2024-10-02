eee48313f749632b79d81dd022509fe85718c406
Bruno Martins <bgcngm@gmail.com>
2024-10-02 16:16:22 +0000

    peridot: Resolve missing symbols on libqcodec2_core
    Change-Id: I300d78733f07b7fe1ba6b5066d02f49eefbeb54a
    
    Signed-off-by: yoriscape <yoriscape.git@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "gettid: 1" "${2}" || echo -e "\ngettid: 1" >> "${2}"
             ;;
+        vendor/lib64/libqcodec2_core.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcodec2_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcodec2_shim.so" "${2}"
+            ;;
     esac
 }
 
